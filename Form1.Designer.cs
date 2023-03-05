6d82c73b2ea7a0d5ca2e19751e0360b0b016bf88
IA7308 <125360024+IA7308@users.noreply.github.com>
2023-03-05 21:55:05 +0700

    Final Versi yang sudah ditambahkan comment

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace TPMOD3_1302210104
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.OutputBoxes = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(564, 101);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(146, 76);
+            this.SubmitButton.TabIndex = 0;
+            this.SubmitButton.Text = "Submit/Kirim";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(69, 101);
+            this.InputBox.Multiline = true;
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(489, 76);
+            this.InputBox.TabIndex = 1;
+            this.InputBox.TextChanged += new System.EventHandler(this.InputBox_TextChanged_1);
+            // 
+            // OutputBoxes
+            // 
+            this.OutputBoxes.Location = new System.Drawing.Point(69, 184);
+            this.OutputBoxes.Multiline = true;
+            this.OutputBoxes.Name = "OutputBoxes";
+            this.OutputBoxes.Size = new System.Drawing.Size(641, 78);
+            this.OutputBoxes.TabIndex = 2;
+            this.OutputBoxes.TextChanged += new System.EventHandler(this.OutputBoxes_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OutputBoxes);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.SubmitButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.TextBox OutputBoxes;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPMOD3_1302210104
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            //Mebuat String yang diambil dari InputBox
+            string input = InputBox.Text;
+            //Mengirimkan Output teks yang diambil dari InputBox
+            OutputBoxes.Text = "Halo "+input;
+        }
+        private void InputBox_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OutputBoxes_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
